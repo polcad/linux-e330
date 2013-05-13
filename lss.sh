2553b9942f25f288b2c434c7303d29ae436593b8
iseijin <jdwvernon@gmail.com>
2013-05-13 14:03:21 +1000

    Update lss.sh

--- lss.sh
+++ lss.sh
@@ -1,4 +1,13 @@
 # Linux Setup Script (LSS)
 # Author: iseijin
 # Date: 13/05/2013
+# Description: Use git to retrieve various Linux Configuration files from a repository on github.
+# Files:
+#       .bashrc
+#       .bash_aliases
+#       .vimrc
+# Dirs:
+#       $HOME/vim/
+
+
 #
