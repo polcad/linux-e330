906fd60b1e8c564d476bddf0797c39ff784ca0eb
lenovo <lenovo@E330.(none)>
2015-01-31 22:52:32 +0100

    Updated bashrc with path to python libs and LD_LIBRARY_PATH

--- setup-pack.sh
+++ setup-pack.sh
@@ -1,5 +1,6 @@
-sudo apt-get install lighttpd php5-fpm php5-common php5-cli php5-cgi 
+sudo apt-get install lighttpd php5 php5-fpm php5-common php5-cli php5-cgi php5-sqlite
 sudo apt-get install gedit-developer-plugins phpmyadmin geany geany-common
+sudo apt-get install php-pear php-apc php5-curl
 # mysqldb for Asterisk eventmanager
 # http://codeinthehole.com/writing/how-to-set-up-mysql-for-python-on-ubuntu/
 apt-get install python-pip python-mysqldb
