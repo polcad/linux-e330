4a6938976769ee77e29b43f1e0ffd2bc7a11db49
lenovo <lenovo@localhost>
2016-05-07 23:30:05 +0200

    adding default actions to mrconfig

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,27 +10,29 @@ trap 'exit 1' ERR
 git_host=github.com
 git_user=polcad
 git_user_at_host=$git_user@$git_host
-MACHINE='linux-e330'
+machine='linux-e330'
+# get the diractory name of the script
+rdir=`dirname $(readlink -f "$0")`
 
 echo "Setting up ~/bin ..."
 if ! [ -d ~/bin ]; then
     mkdir ~/bin
 fi
 
-cd bin
+cd $HOME/bin
 
-if ! [ -d ~/bin/$MACHINE ]; then
-    git clone https://github.com/polcad/$MACHINE.git
+if ! [ -d ~/bin/$machine ]; then
+    git clone https://github.com/polcad/$machine.git
 fi
 
 export PATH=$HOME/bin:$PATH
 
 if ! [ -e ~/.mrtrust ]; then
-    echo "$HOME/bin/$MACHINE/mrconfig" > ~/.mrtrust
+    echo "$HOME/bin/$machine/mrconfig" > ~/.mrtrust
 fi
 
 if ! [ -e ~/.mrconfig ]; then
-    ln -sf "$HOME/bin/$MACHINE/mrconfig" ~/.mrconfig
+    ln -sf "$HOME/bin/$machine/mrconfig" ~/.mrconfig
 fi
 
 echo "Running mr checkout ..."
